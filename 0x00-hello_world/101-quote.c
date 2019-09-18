1de9729cb96529a5cdca12e744f6fe46ba24c5ee
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-09-18 23:26:40 +0000

    Added solution of task 9.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Print a quote on the standard error.
@@ -7,8 +7,6 @@
  */
 int main(void)
 {
-	char error[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	perror(error);
-	return (1);
+	write(2,  "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (-1);
 }
