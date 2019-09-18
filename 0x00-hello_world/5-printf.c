65f247febc25383fd7bd0cdb2724fb0c773dae05
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-09-18 20:03:51 +0000

    Added solution of task 5.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Principal function.
+ * Return: Return 0 value.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
