0fa85d7bb79112fba11810d28a3b4a05e7a94806
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-09-18 22:36:10 +0000

    Added solution of task 9.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print a quote on the standard error.
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	char error[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	perror(error);
+	return (1);
+}
