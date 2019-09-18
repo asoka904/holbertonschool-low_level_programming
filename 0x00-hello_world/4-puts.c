ffeed13878c9dfc812e08b5baab0720e3b5e3574
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-09-18 19:49:04 +0000

    Fixed solution of task 4.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Principal function.
+ */
 int main(void)
 {
 	char myText[] = "\"Programming is like building a multilingual puzzle";
-	puts(myText);
 
+	puts(myText);
 	return (0);
 }
-
