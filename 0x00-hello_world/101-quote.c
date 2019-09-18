5186e589d89e9bc6d51cb629e3487edeff693a04
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-09-18 22:42:25 +0000

    Added solution of task 9.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char error[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char error[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	perror(error);
 	return (1);
