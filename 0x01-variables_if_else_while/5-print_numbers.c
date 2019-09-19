bb8c2eed02cfb7cf43fec65f83ff21067bd0ab07
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-09-19 19:29:33 +0000

    Added solution of task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Print the digits from 0 to 9.
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	int tmp;
+
+	for (tmp = 0; tmp <= 9; tmp++)
+		putchar(tmp);
+	putchar('\n');
+
+	return (0);
+}
