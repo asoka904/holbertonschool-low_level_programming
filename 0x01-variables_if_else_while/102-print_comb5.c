15100489bfa85ed9758ad43043c21c6af9c0084d
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-09-20 00:24:24 +0000

    Added solution of task 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Print the digits from 0 to 99.
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	int tmpU1 = '1';
+	int tmpU2 = '0';
+	int tmpD1 = '0';
+	int tmpD2 = '0';
+
+	for (tmpD2 = '0'; tmpD2 <= '9'; tmpD2++)
+	{
+		for (tmpU2 = '0'; tmpU2 <= '9'; tmpU2++)
+		{
+			for (; tmpD1 <= '9'; tmpD1++)
+			{
+				for (; tmpU1 <= '9'; tmpU1++)
+				{
+					putchar(tmpD2);
+					putchar(tmpU2);
+					putchar(' ');
+					putchar(tmpD1);
+					putchar(tmpU1);
+
+					if (tmpD2 == '9' && tmpU2 == '8')
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+				tmpU1 = '0';
+			}
+			tmpD1 = tmpD2;
+			tmpU1 = tmpU2 + 2;
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
