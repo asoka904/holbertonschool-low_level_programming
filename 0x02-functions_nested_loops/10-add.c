1a7ff886dd08fbee7a5a708f3f3858022194d1b8
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-09-24 14:54:40 +0000

    Added solution of task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,6 +2,8 @@
 
 /**
  * add - Adds two integers and returns the result.
+ * @a: First integer to sum.
+ * @b: Second integer to sum.
  * Return: The sum of the parameters.
  */
 int add(int a, int b)
