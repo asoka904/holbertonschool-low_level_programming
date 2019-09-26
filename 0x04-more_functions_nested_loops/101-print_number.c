f18a397114151209c748f5d0aae5ab737faa8606
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-09-26 23:09:58 +0000

    Added solution of task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,19 +7,20 @@
 
 void print_number(int n)
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		n *= -1;
-	}
-
-	if (n > 9)
+	if (n > 9 || n < -9)
 	{
 		print_number(n / 10);
+		if (n < 0)
+			n = (n % 10) * -1;
 		_putchar ((n % 10) + '0');
 	}
 	else
 	{
+		if (n < 0)
+		{
+			_putchar('-');
+			n *= -1;
+		}
 		_putchar(n + '0');
 	}
 }
