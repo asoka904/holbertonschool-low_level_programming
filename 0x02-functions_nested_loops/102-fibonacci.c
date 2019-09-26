bc985a4e7d26bff71a8f627dd3809422a2a55a91
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-09-26 04:58:11 +0000

    Added solution of task 13

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Print fibonacci series.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long n, first, second, sum;
+
+	first = 2;
+	second = 1;
+	sum = 3;
+	n = 2;
+	printf("1, 2, ");
+	while (n <= 50)
+	{
+		printf("%ul", sum);
+		if (n == 49)
+			break;
+		printf(", ");
+
+		second = first;
+		first = sum;
+		sum = first + second;
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
