d1b3d844516c70ac9dfc6b209d34873f7267d120
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-10-01 20:36:52 +0000

    Added solution of task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,6 +12,9 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		(i < n - 1) ? printf("%i, ", a[i]) : printf("%i\n", a[i]);
+		printf("%i", a[i]);
+		if (i < n - 1)
+			printf(", ");
 	}
+	printf("\n");
 }
