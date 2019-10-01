6aebaaab1149e3ccef2cf3b7ba6f730f01f0c78b
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-10-01 23:14:56 +0000

    Added solution of task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,15 +9,17 @@ void print_rev(char *s)
 {
 	int c = 0;
 
-	while (*(s + c) != '\0')
+	while (*s != '\0')
+	{
+		s++;
 		c++;
-
+	}
+	s--;
 	while (c > 0)
 	{
+		_putchar(*s);
+		s--;
 		c--;
-		_putchar(*(s + c));
 	}
-
-	_putchar('\0');
 	_putchar('\n');
 }
