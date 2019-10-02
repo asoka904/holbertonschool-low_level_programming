5beff314e167ce2a7ce56706faa777197cd3385e
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-10-02 23:28:27 +0000

    Added solution of task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _puts - Prints every other character of a string, starting with the first
+ * character, followed by a new line.
+ * @str: String to print.
+ */
+
+void puts2(char *str)
+{
+	int c = 0;
+
+	while (*(str + c) != '\0')
+	{
+		if (c % 2 == 0)
+			_putchar(*(str + c));
+		c++;
+	}
+	_putchar('\n');
+}
