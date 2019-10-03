e63dc16f402431202ba9b2194c11220a8c09fa2a
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-10-03 23:22:48 +0000

    Added solotion of task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @s: Pointer to the input string
+ *
+ * Return: Pointer to the string changed.
+ */
+char *cap_string(char *s)
+{
+	int len = 0;
+
+	while (s[len + 1] != '\0')
+	{
+		switch (s[len])
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '\?':
+			case '!':
+			case '\"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+				if (s[len + 1] >= 'a' && s[len + 1] <= 'z')
+					s[len + 1] -= ' ';
+				break;
+		}
+		len++;
+	}
+	return (s);
+}
