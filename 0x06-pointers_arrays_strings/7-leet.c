d0417752f65c79643d3da166a5d172c3aff54ada
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-10-04 00:49:20 +0000

    Added solotion of task 6

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * leet - Encodes a string into 1337.
+ * @s: String in natural language (without encode).
+ *
+ * Return: Pointer to the string encoded.
+ */
+char *leet(char *s)
+{
+	char src[] = "aAeEoOtTlL";
+	char dest[] = "4433007711";
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == src[j])
+			{
+				s[i] = dest[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
