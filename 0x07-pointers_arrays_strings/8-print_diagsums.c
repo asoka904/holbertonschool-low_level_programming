f2d0093010f7a0ce7a16e79714e430eb31ddf639
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-10-08 02:46:03 +0000

    Added solution of task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 
 /**
- * print_diagsums - Prints the sum of the two diagonals of a square matrix of integers.
+ * print_diagsums - Prints the sum of the two diagonals of a square matrix of
+ * integers.
  * @a: Matrix of integers.
  * @size: size of the matrix
  */
@@ -12,12 +13,12 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
-			{
-				if (i == j)
-					sA += a[(i + (size * j))];
-				if ((i + j) == (size - 1))
-					sB += a[i + (size * j)];
-			}
+		{
+			if (i == j)
+				sA += a[(i + (size * j))];
+			if ((i + j) == (size - 1))
+				sB += a[i + (size * j)];
+		}
 	}
 	printf("%i, %i\n", sA, sB);
 }
