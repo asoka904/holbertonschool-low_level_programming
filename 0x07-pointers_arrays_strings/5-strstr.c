f87e33916b4df64a0e6cac7e03a39fb3d40ced88
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-10-08 03:46:48 +0000

    Added solution of task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,5 +10,29 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
+	int i;
+	char *p = NULL;
+
+	for (i = 0; haystack[i] != '\n'; i++)
+	{
+		if (haystack[i] == needle[0])
+			p = compare((haystack + i), needle);
+		if (p != NULL)
+			return (p);
+	}
+
 	return (NULL);
+}
+
+char *compare(char *s1, char *s2)
+{
+	int i;
+
+	/* OJO: si s1 llega al final y s2 no!! */
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+		if (s1[i] != s2[i])
+		{
+			return (NULL);
+		}
+	return (s1);
 }
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 int _putchar(char c);
+char *compare(char *s1, char *s2);
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
