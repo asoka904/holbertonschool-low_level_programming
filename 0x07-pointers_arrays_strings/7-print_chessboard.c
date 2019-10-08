c05a37f34f58a7363f6f7deb04be471e666d6153
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-10-08 01:28:07 +0000

    Added solution of task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - Prints the chessboard.
+ * @a: Chess board (Array multidimentional).
+ */
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i, j;
+
+	for (i = 0; i < 8; i++)
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+}
