440f44a70c9d34241acffce550d5f7431c7cfa15
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-10-09 14:14:43 +0000

    Initial commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - Prints a string.
+ * @s: String to print
+ */
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	if (*s == '\0')
+		_puts_recursion(s + 1);
+
+	return;
+}
