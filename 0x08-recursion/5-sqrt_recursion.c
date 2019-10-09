a9620ed0b23c28cc381835c5dc4d65a0ba05d378
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-10-09 17:56:15 +0000

    Added solution of task 0

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - Calculate the natural square root of a number.
+ * @n: Integer
+ *
+ * Return: the natural square root of a number. If n does not have a natural
+ * square root, the function should return -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (y < 0)
+		return (-1);
+}
+
+/**
+ * isroot - Calculate the  scuare root 
+ *
+ */
+
