404df943515f8edc8475dc7653e83b851f2eb629
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-10-09 18:36:19 +0000

    Added solution of task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,12 +9,27 @@
  */
 int _sqrt_recursion(int n)
 {
-	if (y < 0)
-		return (-1);
+	return (isroot(n, 0));
 }
 
 /**
- * isroot - Calculate the  scuare root 
- *
+ * isroot - Find the scuare root of a number.
+ * Description: Check recursibely the root of an number, untul to find it if
+ * it exist.
+ * @n: number to find the root
+ * @s: possible root (Optional)
+ * Return: the root scuare of n. If it do not exist, return -1
  */
+int isroot(int n, int s)
+{
+	if (n < 0)
+		return (-1);
+	
+	if (n < s)
+		return (-1);
 
+	if (s * s == n)
+		return (s);
+
+	return (isroot(n, s + 1));
+}
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -14,5 +14,6 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
+int isroot(int n, int s);
 #endif /* HOLBERTON_H */
 
