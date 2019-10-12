6364a488d9a9e7a3cb40e37b745440d3200106ce
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-10-12 20:20:48 +0000

    Added solution of task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,6 +2,8 @@
 
 /**
  * main - Prints all arguments it receives.
+ * @argc: Number of arguments (including name of file).
+ * @argv: Arguments of the function
  *
  * Return: Always 0.
  */
@@ -10,7 +12,7 @@ int main(int argc, char *argv[])
 	int i;
 
 	for (i = 0; i < argc; i++)
-	printf("%s\n", argv[0]);
+		printf("%s\n", argv[0]);
 
 	return (0);
 }
