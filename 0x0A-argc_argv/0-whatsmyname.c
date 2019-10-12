24401f0c71f1fe499f598cf8c9bffae6f0fd69bb
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-10-12 20:14:07 +0000

    Added solution of task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,8 @@
 
 /**
  * main - Program that prints its name
- *
+ * @argc: Number of arguments (including name of file).
+ * @argv: Arguments of the function
  * Return: Always 0.
  */
 int main(int argc __attribute__((unused)), char *argv[])
