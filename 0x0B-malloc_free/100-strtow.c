17e3ccfa51880c3209dbd54298a102d77fa1edca
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-10-16 02:52:28 +0000

    Added solution of task 6

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,11 @@
+/**
+ * strtow - Splits a string into words.
+ * @str: string to split
+ *
+ * Return: returns a pointer to an array of strings (words)
+ */
+char **strtow(char *str)
+{
+	if (str == NULL || str == "")
+		return (NULL);
+}
