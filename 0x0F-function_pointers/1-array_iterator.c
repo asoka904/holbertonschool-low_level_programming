51368662775ad987d8222600703e8abdaea01735
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-10-23 16:39:22 +0000

    Added solution of task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,2 +1,16 @@
+#include "function_pointers.h"
+
 /**
- *
+ * array_iterator - Executes a function given as a parameter on each element of
+ * an array.
+ * @array: Array of elements to use in the function action
+ * @size: size of the array
+ * @action: Pointer to the function to use in each element of the array
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		(*action)(array[i]);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,11 +3,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
+/*typedef int size_t;*/
 int _putchar(char c);
 int _strlen(char *s);
 #endif
