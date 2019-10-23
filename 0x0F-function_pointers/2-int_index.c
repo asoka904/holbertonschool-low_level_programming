ef1f28a6155eb51f361d7ff3d59604bc71adab60
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-10-23 19:56:47 +0000

    Added solution of task 1

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,18 +6,19 @@
  * @array: Array of integers to search
  * @size: Size of the array
  * @cmp: Function that indicate the condition
+ *
+ * Return: Return the index of the first integer in the array that meet the
+ * condition of the function cmp. Return -1 in other case.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
-	if (array == NULL || cmp == NULL)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
-		if((*cmp)(array[i]) != 0)
+		if ((*cmp)(array[i]) != 0)
 			return (i);
 	return (-1);
 }
