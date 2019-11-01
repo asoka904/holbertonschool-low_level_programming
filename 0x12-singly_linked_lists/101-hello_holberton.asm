221791adda9ac4848ea626fcd75560cff6678a76
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-11-01 04:54:58 +0000

    101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,14 @@
-.data
-format:   .ascii "%d\n"  
+global main
 
-.text
-.global main  
+extern printf
+
+txt db 0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x2C, 0x20, 0x48, 0x6F, 0x6C, 0x62, 0x65, 0x72, 0x74, 0x6F, 0x6E, 0x0A
+
+section .text
 main:
-mov    $format, %rbx
-mov    (%rbx), %rdi
-mov    $1, %rsi
-call     printf
+mov 	rdi, txt
+mov	rax, 0
+call	printf
+mov	rax, 0
 ret
+
