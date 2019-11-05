a9fcfc5c7f169eeb88d6b6566e20ede87c9bf803
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-11-05 22:51:37 +0000

    Added solution fo task 12

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - Prints all the elements of a listint_t list.
+ * @head: First element of a listint_t
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	unsigned int count = 0;
+
+	if (head == NULL)
+		exit(98);
+	for (; head != NULL; count++)
+	{
+		printf("[%p] %i\n", (void *)head, head->n);
+		head = head->next;
+	}
+
+	return (count);
+}
