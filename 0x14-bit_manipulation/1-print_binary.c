f7dc648b8134ccf0bee9360ea6e1f8e7f3d56841
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-11-07 22:12:50 +0000

    Added task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_binary - Prints the binary representation of a number.
+ * @n: number to print
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int i = 0, mask = 1, cpy = n;
+
+	if (n == 0)
+	{
+		_putchar(48);
+		return;
+	}
+	
+	for (; cpy; i++)
+		cpy >>= 1;
+	mask <<= --i;
+	while (mask)
+	{
+		if (n & mask)
+			_putchar(49);
+		else
+			_putchar(48);
+		mask >>= 1;
+	}
+}
