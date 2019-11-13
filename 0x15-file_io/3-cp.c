a5eb9a9660c0376b86d7a30a53ad1d1b15a92f09
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-11-13 05:19:25 +0000

    Added task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * main - Program that copies the content of a file to another file.
+ * @ac: Number of args
+ * @av: Arguments of the function
+ *
+ * Return: 0 in success.
+ */
+int main()
+{
+	return (0);
+}
