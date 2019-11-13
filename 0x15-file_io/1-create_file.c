ff080d2814979cfb0842e4a352b23805ed91920f
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-11-13 04:53:47 +0000

    Added task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * create_file - creates a file.
+ * @filename: Name of the file to create
+ * @text_content: content towrite in the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file = 0;
+	int writed = 0;
+	int len;
+
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+	if (file < 0)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
+
+		writed = write(file, text_content, len);
+		if (write < 0)
+			return (-1);
+	}
+
+	close(file);
+	return (1);
+}
