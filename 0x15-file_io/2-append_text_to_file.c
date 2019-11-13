a5dc8188a3eb7533848305f0cae3b9960947803b
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-11-13 05:12:22 +0000

    Added task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	int len;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
 	file = open(filename, O_APPEND | O_WRONLY);
 	if (file == -1)
