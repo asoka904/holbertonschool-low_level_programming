829f59b36e3517375d4eeeaa05c3127c198566cf
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2019-11-13 05:06:27 +0000

    Added task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: Name of the file to append
+ * @text_content: content to write in the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file = 0;
+	int writed = 0;
+	int len;
+
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_APPEND | O_WRONLY);
+	if (file < 0)
+		return (-1);
+
+	if (text_content)
+	{
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
+
+		writed = write(file, text_content, len);
+		if (writed < 0)
+			return (-1);
+	}
+
+	close(file);
+	return (1);
+}
