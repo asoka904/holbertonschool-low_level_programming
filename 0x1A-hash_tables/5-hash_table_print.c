7dda3807b2909b7c425bc1326a1d567fe0547ecd
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-01-20 06:53:27 +0000

    Updated

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table
+ * @ht: is the hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *table;
+	char *buf = "";
+	unsigned long int i;
+
+	if (ht)
+	{
+		printf("{");
+		for (i = 0; i < ht->size; i++)
+		{
+			table = ht->array[i];
+			while (table)
+			{
+				printf("%s'%s': '%s'", buf, table->key, node->value);
+				table = table->next;
+				buf = ", ";
+			}
+		}
+		printf("}\n");
+	}
+}
