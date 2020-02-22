501341a8c98c40c51d89d7deafc17f1b8fb152cc
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-02-22 01:51:33 +0000

    Added task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module contain the island_perimeter function
+"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid
+    Args:
+        grid: list of list of integers
+    """
+    return(0)
