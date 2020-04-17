fe446683f5e9c0a777a9c5067b4098c680885845
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-04-17 06:12:54 +0000

    Added task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,5 +14,12 @@ int binary_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+
+		if (array[i] == value)
+			return (i);
+	}
 	return (-1);
 }
