52cac355b572946dbf6bbbf37134c236dcdae078
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-04-17 06:06:24 +0000

    Added prototypes of the functions

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+/**
+ * binary_search - Searches for a value in a sorted array of integers using
+ * the Binary search algorithm
+ * @array: array of integers
+ * @size: size of the array
+ * @value: the value to search for
+ *
+ * Return: index where is found.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	return (0);
+}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+/**
+ * binary_search - Searches for a value in a sorted array of integers using
+ * the Binary search algorithm
+ * @array: input
+ * @size: the size of the array
+ * @value: the value to search
+ *
+ * Return: the index of the value, or -1 if doesn't exist
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	return (0);
+}
