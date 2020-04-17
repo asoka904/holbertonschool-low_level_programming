7a9b26edda5c65499a589159a6bbd3ce3723e158
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-04-17 06:34:16 +0000

    Added task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,5 +10,27 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
+	unsigned int start, mid, end, i;
+
+	if (array == NULL)
+		return (-1);
+	start = 0;
+	end = size - 1;
+
+	while (start <= end)
+	{
+		printf("Searching in array: ");
+		for (i = start; i < end; i++)
+			printf("%i, ", array[i]);
+		printf("%i\n", array[i]);
+
+		mid = (start + end) / 2;
+		if (value > array[mid])
+			start = mid + 1;
+		else if (value < array[mid])
+			end = mid - 1;
+		else
+			return (mid);
+	}
 	return (-1);
 }
