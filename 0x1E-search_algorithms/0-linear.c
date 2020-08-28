a3fedabc4317db37a4ab9e683aa4d4daae53a642
Jose Saldarriaga <jose.saldarriaga@outlook.com>
2020-08-28 00:25:07 -0500

    Fixed

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,14 +1,14 @@
 #include "search_algos.h"
 /**
- * binary_search - Searches for a value in a sorted array of integers using
- * the Binary search algorithm
+ * linear_search - Searches for a value in a sorted array of integers using
+ * the linear search algorithm
  * @array: array of integers
  * @size: size of the array
  * @value: the value to search for
  *
  * Return: index where is found.
  */
-int binary_search(int *array, size_t size, int value)
+int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
